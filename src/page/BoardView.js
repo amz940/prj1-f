7518d8cf05ac40abe4c7b01ad609e4ddd3d5b797
amz940 <dh_bug@naver.com>
2023-11-09 16:44:32 +0900

    게시물 삭제 기능

--- src/page/BoardView.js
+++ src/page/BoardView.js
@@ -1,12 +1,30 @@
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
 import axios from "axios";
-import { Box, FormControl, FormLabel, Input, Spinner } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  FormControl,
+  FormLabel,
+  Input,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+  Spinner,
+  useDisclosure,
+  useToast,
+} from "@chakra-ui/react";
 
 export function BoardView() {
   const { id } = useParams();
-
+  const { isOpen, onOpen, onClose } = useDisclosure();
   const [board, setBoard] = useState(null);
+  const navigate = useNavigate();
+  const toast = useToast();
 
   useEffect(() => {
     axios
@@ -17,6 +35,26 @@ export function BoardView() {
   if (board === null) {
     return <Spinner />;
   }
+
+  function handleDelete() {
+    axios
+      .delete("/api/board/remove/" + id)
+      .then((response) => {
+        toast({
+          description: id + "번 게시물이 삭제되었습니다",
+          status: "success",
+        });
+        navigate("/");
+      })
+      .catch((error) => {
+        toast({
+          description: "삭제 중 문제가 발생하였습니다",
+          status: "error",
+        });
+      })
+      .finally(() => onClose());
+  }
+
   return (
     <Box>
       <h1>{board.id}번 글 보기</h1>
@@ -36,6 +74,27 @@ export function BoardView() {
         <FormLabel>작성일시</FormLabel>
         <Input value={board.inserted}></Input>
       </FormControl>
+      <Button colorScheme="purple">수정</Button>
+      <Button colorScheme="red" onClick={onOpen}>
+        삭제
+      </Button>
+
+      {/*삭제 모달*/}
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>삭제 확인</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>삭제 하시겠습니까?</ModalBody>
+
+          <ModalFooter>
+            <Button onClick={onClose}>닫기</Button>
+            <Button onClick={handleDelete} colorScheme="red">
+              삭제하기
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
     </Box>
   );
 }
