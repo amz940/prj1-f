afe2d3312afe1bdae4eec99cd70334262e3c39b6
amz940 <dh_bug@naver.com>
2023-11-09 11:12:16 +0900

    프로젝트 기본 틀 마련

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
-function App() {
-  return (
-    <div>
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+import { BoardList } from "./page/BoardList";
+import { BoardWrite } from "./page/BoardWrite";
+import { HomeLayout } from "./layout/HomeLayout";
 
-    </div>
-  );
+const routes = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<HomeLayout />}>
+      {/*localhost:3000 경로*/}
+      <Route index element={<BoardList />} />
+      <Route path="write" element={<BoardWrite />} />
+    </Route>,
+  ),
+);
+
+function App() {
+  return <RouterProvider router={routes} />;
 }
 
 export default App;
