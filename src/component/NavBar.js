c056fabbcf5d4717b4efc2ca6ca393c49a2f3db2
amz940 <dh_bug@naver.com>
2023-11-14 15:21:36 +0900

    로그인 여부에 따라 navbar 버튼 출력 결정

--- src/component/NavBar.js
+++ src/component/NavBar.js
@@ -1,24 +1,45 @@
-import { Button, Flex } from "@chakra-ui/react";
+import { Button, Flex, useToast } from "@chakra-ui/react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
+import { useContext } from "react";
+import { LoginContext } from "../App";
 
 export function NavBar() {
   const navigate = useNavigate();
+  const { fetchLogin, login, isAuthenticated } = useContext(LoginContext);
+  const toast = useToast();
 
   function handleLogout() {
     // TODO : 로그아웃 후 할 일 추가
-    axios.post("/api/member/logout").then(() => console.log("로그아웃 성공"));
+    axios
+      .post("/api/member/logout")
+      .then(() => {
+        toast({
+          description: "로그아웃 되었습니다",
+          status: "info",
+        });
+        navigate("/");
+      })
+      .finally(() => fetchLogin());
   }
 
   return (
     <Flex>
       <Button onClick={() => navigate("/")}>home</Button>
-      <Button onClick={() => navigate("/write")}>write</Button>
-      <Button onClick={() => navigate("/signup")}>signup</Button>
-      <Button onClick={() => navigate("/member/list")}>회원목록</Button>
-      <Button onClick={() => navigate("/login")}>로그인</Button>
+      {isAuthenticated() && (
+        <Button onClick={() => navigate("/write")}>write</Button>
+      )}
+      {isAuthenticated() || (
+        <Button onClick={() => navigate("/signup")}>signup</Button>
+      )}
+      {isAuthenticated() && (
+        <Button onClick={() => navigate("/member/list")}>회원목록</Button>
+      )}
+      {isAuthenticated() || (
+        <Button onClick={() => navigate("/login")}>로그인</Button>
+      )}
       {/*로그아웃은 따로 창을 만들 필요 없이 바로 하면 되기 떄문에 함수 작성*/}
-      <Button onClick={handleLogout}>로그아웃</Button>
+      {isAuthenticated() && <Button onClick={handleLogout}>로그아웃</Button>}
     </Flex>
   );
 }
