9735dd96215104ac641d53670a59a34dbd8fad9d
amz940 <dh_bug@naver.com>
2023-11-15 11:44:25 +0900

    댓글 쓰는 중 상태(isSubmitting)을 CommentContainer로 옮김

--- src/component/CommentContainer.js
+++ src/component/CommentContainer.js
@@ -1,47 +1,94 @@
-import { Box, Button, Input, Textarea } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  Card,
+  CardBody,
+  CardHeader,
+  Flex,
+  Heading,
+  Input,
+  Stack,
+  StackDivider,
+  Textarea,
+  Text,
+} from "@chakra-ui/react";
 import axios from "axios";
 import { useEffect, useState } from "react";
 
-function CommentForm({ boardId }) {
-  const [comment, setComment] = useState(null);
+function CommentForm({ boardId, isSubmitting, onSubmit }) {
+  const [comment, setComment] = useState("");
 
   function handleSubmit() {
-    axios.post("/api/comment/add", {
-      boardId,
-      comment,
-    });
+    onSubmit({ boardId, comment });
   }
 
   return (
     // 여러 줄 쓰고 싶을떈 Textarea / 한 줄은 input
     <Box>
       <Textarea value={comment} onChange={(e) => setComment(e.target.value)} />
-      <Button onClick={handleSubmit}>쓰기</Button>
+      <Button isDisabled={isSubmitting} onClick={handleSubmit}>
+        쓰기
+      </Button>
     </Box>
   );
 }
 
 function CommentList({ boardId }) {
-  const [commentList, setCommentList] = useState(null);
+  const [commentList, setCommentList] = useState([]);
 
   useEffect(() => {
     const params = new URLSearchParams();
     params.set("id", boardId);
+
     axios
       .get("/api/comment/list?" + params)
       .then((response) => setCommentList(response.data));
   }, []);
 
   return (
-    // TODO : 댓글 read 기능 추가
-    <Box>댓글 리스트</Box>
+    <Card>
+      <CardHeader>
+        <Heading size={"md"}>댓글 리스트</Heading>
+      </CardHeader>
+      <CardBody>
+        <Stack divider={<StackDivider />} spacing="4">
+          {/*TODO : 새로운 줄 출력*/}
+          {/*TODO : 댓글 작성 후 re render*/}
+          {commentList.map((comment) => (
+            <Box>
+              <Flex>
+                <Heading size="xs">{comment.memberId}</Heading>
+                <Text fontsize="xs">{comment.inserted}</Text>
+              </Flex>
+              <Text pt="2" fontsize="xs">
+                {comment.comment}
+              </Text>
+            </Box>
+          ))}
+        </Stack>
+      </CardBody>
+    </Card>
   );
 }
 
 export function CommentContainer({ boardId }) {
+  const [isSubmitting, setIsSubmitting] = useState(false);
+
+  function handleSubmit(comment) {
+    setIsSubmitting(true);
+
+    axios
+      .post("/api/comment/add", comment)
+      .finally(() => setIsSubmitting(false));
+  }
+
   return (
     <Box>
-      <CommentForm boardId={boardId} />
+      <CommentForm
+        boardId={boardId}
+        isSubmitting={isSubmitting}
+        onSubmit={handleSubmit}
+      />
       <CommentList boardId={boardId} />
     </Box>
   );
