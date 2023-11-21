de067cd286f8757a26263e82f47bea91cc4e526f
amz940 <dh_bug@naver.com>
2023-11-21 09:22:11 +0900

    파일 업로드 속성명 변경 ( files -> uploadFiles)

--- src/page/board/BoardWrite.js
+++ src/page/board/BoardWrite.js
@@ -16,7 +16,7 @@ export function BoardWrite() {
   const [title, setTitle] = useState("");
   const [content, setContent] = useState("");
   const [isSubmitting, setIsSubmitting] = useState(false);
-  const [files, setFiles] = useState(null);
+  const [uploadFiles, setUploadFiles] = useState(null);
 
   const toast = useToast();
   const navigate = useNavigate();
@@ -27,7 +27,7 @@ export function BoardWrite() {
       .postForm("/api/board/add", {
         title,
         content,
-        files,
+        uploadFiles,
       })
       .then(() => {
         toast({
@@ -76,7 +76,7 @@ export function BoardWrite() {
             type="file"
             accept="image/*"
             multiple
-            onChange={(e) => setFiles(e.target.files)}
+            onChange={(e) => setUploadFiles(e.target.files)}
           />
           <FormHelperText>
             한개 파일은 1MB 이내, 총 파일은 10MB 이내로 첨부하시오.
